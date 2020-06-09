45a591acfcd4f41734c6823dc3d9e3034935082c
eden dadon <edenvvv@gmail.com>
2020-06-09 20:06:23 +0300

    .

---
+++ threads/CourierTournament.java
@@ -0,0 +1,13 @@
+package threads;
+
+import animals.Animal;
+public class CourierTournament extends Tournament {
+    @Override
+    public void setup(Animal[][] setup_arr) {
+        Boolean startFlag;
+        Scores scores;
+        for (int i=0; i < setup_arr.length; ++i){
+            Boolean Flag = false;
+        }
+    }
+}
